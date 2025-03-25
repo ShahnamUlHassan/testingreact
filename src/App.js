040121af9f046da11cfde91c771f27a3eed7a713
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-03-25 13:44:27 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Navbar from './comp/navbar'
+import Ahmad from  './asset/ahmad.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function abc() {
+  alert(`hi ahmad`)
 }
 
-export default App;
+let name = "hassan"
+
+  return(
+    <div>
+
+{/* html */}
+<img src={Ahmad} alt="logo"/>
+
+
+<Navbar />
+<Navbar />
+<Navbar />
+<h1 className='ab'>{name}</h1>
+<h1>Welcome to React!</h1>
+<p>This is a paragraph of text.</p>
+
+<button>Click Me!</button>
+<label>Welcome to React!</label>
+<input type="text" placeholder='enter your name'/>
+<button type='submit' onClick={abc}>check now</button>
+
+    </div>
+
+
+
+  )
+
+
+
+}
+
+
+
+export default App
